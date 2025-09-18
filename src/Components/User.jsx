087979dc1fec06d82848321cff9949f11bff46f6
Siddique Ahmed <developersiddique123@gmail.com>
2025-09-18 06:50:42 +0600

    starting the chat project

---
+++ src/Components/User.jsx
@@ -0,0 +1,21 @@
+import { IoSearchOutline } from "react-icons/io5";
+
+const User = () => {
+  return (
+    <>
+      <section className="container">
+        <div>
+          <div>
+            <div>
+              <IoSearchOutline />
+              <input type="search" />
+            </div>
+          </div>
+          <div></div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default User;
